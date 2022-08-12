ef0887bd29c427a7ee7aacb1a0c969e5c6f81a73
Denis Bernovich <denisbernovich@gmail.com>
2022-08-12 16:54:17 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = "";
+  if (str.length == 0) return brack
+  for (let i = 0; i < bracketsConfig.length; i++) {
+   let bracket = bracketsConfig[i].reduce((a, b) => a + b )
+   let n = str.indexOf(bracket);
+   brack = str.includes(bracket);
+   newStr = brack ? str.slice(0, n) + str.slice( n + 2) : "";
+   if (brack) break;
+ }
+ return check (newStr, bracketsConfig, brack);
 }
