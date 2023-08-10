9bdde10a8f1f920857f56875f486c73912ecc06e
Bernovich Denis <denisbernovich@gmail.com>
2023-08-10 17:49:14 +0300

    fix: code

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ module.exports = function check(str, bracketsConfig) {
   let newStr = "";
   if (str.length == 0) return brack
   for (let i = 0; i < bracketsConfig.length; i++) {
-   let bracket = bracketsConfig[i].reduce((a, b) => a + b )
-   let n = str.indexOf(bracket);
-   brack = str.includes(bracket);
-   newStr = brack ? str.slice(0, n) + str.slice( n + 2) : "";
-   if (brack) break;
- }
- return check (newStr, bracketsConfig, brack);
+    let bracket = bracketsConfig[i].reduce((a, b) => a + b)
+    let n = str.indexOf(bracket);
+    brack = str.includes(bracket);
+    newStr = brack ? str.slice(0, n) + str.slice(n + 2) : "";
+    if (brack) break;
+  }
+  return check(newStr, bracketsConfig, brack);
 }
